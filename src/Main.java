31026d1df772fb300a893b5804330e4d6bd2e534
RomanIgambaev <roman.igambaev11@gmail.com>
2023-09-16 20:06:13 +0500

    Caesar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
+    public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                result.append((char) ((ch - base + shift) % 26 + base));
+            } else {
+                result.append(ch);
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static String decrypt(String text, int shift) {
+        return encrypt(text, 26 - shift);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String plaintext = "Hello, World!";
+        int shift = 3;
+
+        String encryptedText = encrypt(plaintext, shift);
+        System.out.println("Зашифрованный текст: " + encryptedText);
+
+        String decryptedText = decrypt(encryptedText, shift);
+        System.out.println("Расшифрованный текст: " + decryptedText);
     }
 }
